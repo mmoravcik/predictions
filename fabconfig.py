daf9f871937f4f8b6b4bac946e45afa439acb35a
Matus Moravcik <matus.moravcik@gmail.com>
2012-02-24 14:58:04 +0000

    corrected fab

--- fabconfig.py
+++ fabconfig.py
@@ -10,8 +10,8 @@ def dev():
     env.hosts = ['178.77.101.195']
     env.virtualenv = env.main_dir + 'virtualenvs/dev'
     env.code_dir = env.main_dir + 'builds/dev'
-    env.apache_conf = 'deploy/apache/dev.conf'
-    env.nginx_conf = 'deploy/nginx/dev.conf'
+    env.apache_conf = 'deploy/apache/predictions.fantiago.com.conf'
+    env.nginx_conf = 'deploy/nginx/predictions.fantiago.com.conf'
     env.wsgi = 'deploy/wsgi/dev.wsgi'
 
 def stage():
